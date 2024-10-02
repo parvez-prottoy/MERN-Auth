b0147e9e4866e92720a03481d4d2d9ca9fd5ff7f
parvez-prottoy <parvezahmedprottoy.web@gmail.com>
2024-10-02 19:59:11 +0600

    added some middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,17 @@
+import cors from "cors";
 import express from "express";
+import morgan from "morgan";
 import connectDb from "./config/db.js";
 
 const app = express();
 // note: connect db
 connectDb.apply();
+// note: middleware
+app.use([
+  morgan("dev"),
+  cors(),
+  express.json(),
+  express.urlencoded({ extended: true }),
+]);
 
 export default app;
